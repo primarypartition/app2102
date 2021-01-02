7466666ab92ce1e33ddcc61f5491cff2a184ce87
Your Name <you@example.com>
2021-01-01 19:05:44 -0700

    quiz questions

--- resources/views/backend/quiz/question.blade.php
+++ resources/views/backend/quiz/question.blade.php
@@ -1,94 +1,54 @@
 @extends('backend.layouts.master')
 
-	@section('title','quiz questions')
+@section('title', 'quiz questions')
 
-	@section('content')
+@section('content')
+<div class="span9">
+    <div class="content">
+        @foreach($quizzes as $quiz)
+            <div class="module">
+                <div class="module-head">
+                    <h3>{{$quiz->name}}</h3>
+                </div>
 
-				<div class="span9">
-                    <div class="content">
+                <div class="module-body">
+                    <p><h3 class="heading"></h3></p>
 
-                    	 <!--foreach-->
-                         @foreach($quizzes as $quiz)
+                    <div class="module-body table">
+                        @foreach($quiz->questions as $ques)
+                        <table class="table table-message">
+                        <tbody>
+                            <tr class="read">
+                                {{$ques->question}}
 
-                        <div class="module">
-                            <div class="module-head">
-                                
-                                  <h3>{{$quiz->name}}</h3>
+                                <td class="cell-author hidden-phone hidden-tablet">
+                                    @foreach($ques->answers as $answer)
+                                        <p>
+                                            {{$answer->answer}}
 
-                            </div>
-
-                            <div class="module-body">
-                            	
-
-                            <p><h3  class="heading"></h3></p>
-
-
-                             
-
-                                <div class="module-body table">
-                                     <!--foreach-->
-                                     @foreach($quiz->questions as $ques)
-
-                                    <table class="table table-message">
-                                        <tbody>
-                                           
-                                               
-                                            
-                                            
-                                            <tr class="read">
-                                                 
-                                                 {{$ques->question}}
-                                                
-                                                <td class="cell-author hidden-phone hidden-tablet">
-                                                     <!--foreach-->
-                                                     @foreach($ques->answers as $answer)
-                                                        <p>
-                                                            {{$answer->answer}} 
-                                                            
-                                                            @if($answer->is_correct)
-                                                           <span class="badge badge-success"> 
-                                                            correct answer
-                                                           </span>
-                                                           @endif
-                                                            
-                                                        </p>
-                                                        @endforeach
-                                                    <!--endforeach-->
-                                                </td>
-
-                                                
-
-                                               
-                                            </tr>
-                                           
-
-
-                                        </tbody>
-
-                                    </table>
+                                            @if($answer->is_correct)
+                                                <span class="badge badge-success">
+                                                    correct answer
+                                                </span>
+                                            @endif
+                                        </p>
                                     @endforeach
-                                    <!--endforeach-->
-                                </div>
-                              @endforeach
-                            <!--endforeach-->
+                                </td>
+                            </tr>
+                        </tbody>
+                        </table>
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+        @endforeach
 
-                                <div class="module-foot">
-                                <td>
-                                    <a href="{{route('quiz.index')}}"><button class="btn btn-inverse pull-center">Back</button></a>
-                                 </td>
-                                </div>
-
-                       	
-                            </div>
-                   		</div>
-                		
-                		</div>
-           			 
-           			</div>
-        		</div> 
-
-            </div> 
-            </div> 
-
+        <div class="module-foot">
+            <a href="{{route('quiz.index')}}">
+                <button class="btn btn-inverse pull-center">Back</button>
+            </a>
+        </div>
+    </div>
+</div>
 
 @endsection
