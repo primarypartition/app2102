35b635c5e19e0ffb52d7ce195484d9c83ca766bc
Your Name <you@example.com>
2021-01-01 21:08:44 -0700

    seeder, middleware

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
+use App\Models\User;
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -14,5 +16,18 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        $admin = new User();
+    	$admin->name = "admin";
+    	$admin->email = "admin123@gmail.com";
+    	$admin->password = bcrypt('password');
+    	$admin->visible_password = "password";
+    	$admin->email_verified_at = NOW();
+    	$admin->occupation = "CEO";
+    	$admin->address = "Australia";
+    	$admin->phone = "03453494";
+        $admin->is_admin= 1;
+
+    	$admin->save();
     }
 }
